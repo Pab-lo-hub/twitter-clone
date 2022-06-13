f3a18f98a464bf8cf9c5ef5dcce5a40cf72535df
Pab-lo <salaberry.pablo@gmail.com>
2022-06-13 11:57:29 +0200

    new functions added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['i.ibb.co', 'cloudflare-ipfs.com', 'localhost'],
+  },
 }
-
-module.exports = nextConfig
